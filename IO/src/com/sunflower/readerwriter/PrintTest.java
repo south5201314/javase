a68c7a3a7e22e620038e789b11bcc90f432fcf93
sunflower_glb <sunflower_glb@sunflower.com>
2021-02-24 18:19:51 +0800

    IO

--- IO/src/com/sunflower/readerwriter/PrintTest.java
+++ IO/src/com/sunflower/readerwriter/PrintTest.java
@@ -3,27 +3,34 @@ package com.sunflower.readerwriter;
 import java.io.*;
 
 public class PrintTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        PrintStream printTest = null;
         try {
-            System.setOut(new PrintStream("printFile.txt"));
-            while (true){
+            printTest = new PrintStream("printTest.txt");//打印流
+            System.setOut(new PrintStream("printFile.txt"));//重新设置打印输出位置
+            while (true) {
                 String line = bfr.readLine();
-                if("exit".equals(line)||"e".equals(line)){
+                if ("exit".equals(line) || "e".equals(line)) {
                     break;
                 }
                 System.out.println(line);
+                printTest.println(line);
                 System.out.flush();
+                printTest.flush();
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if(bfr !=null){
+            if (bfr != null) {
                 try {
                     bfr.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+            if (printTest != null) {
+                printTest.close();
             }
         }
 
